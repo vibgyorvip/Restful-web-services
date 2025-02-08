76183327c502523fa471d70650780d2b9be8819f
vibgyorvip <vibgyorvipul786@gmail.com>
2025-02-08 18:52:18 +0530

    Internationalization i18n

--- src/main/java/com/programmingpointer/rest/webservices/restful_web_services/HelloController.java
+++ src/main/java/com/programmingpointer/rest/webservices/restful_web_services/HelloController.java
@@ -1,12 +1,22 @@
 package com.programmingpointer.rest.webservices.restful_web_services;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloController {
+
+    private MessageSource messageSource;
+
+    public HelloController(MessageSource messageSource){
+        this.messageSource=messageSource;
+    }
 
     @GetMapping("/hello-world")
     public String helloWorld(){
@@ -21,5 +31,12 @@ public class HelloController {
     @GetMapping("hello-world/{name}")
     public HelloWorldBean helloWorldBeanPath(@PathVariable String name){
         return  new HelloWorldBean(String.format("Hello Good Morning , %s" , name));
+    }
+
+
+    @GetMapping("/hello-world-internationalized")
+    public String helloWorldInternationalized(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
     }
 }
