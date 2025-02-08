71bdb28ba15e892d7f9182152f5cb61d09f3bcd2
vibgyorvip <vibgyorvipul786@gmail.com>
2025-02-08 20:23:55 +0530

    Versining REST API

---
+++ src/main/java/com/programmingpointer/rest/webservices/restful_web_services/versioning/PersonV2.java
@@ -0,0 +1,25 @@
+package com.programmingpointer.rest.webservices.restful_web_services.versioning;
+
+public class PersonV2 {
+
+    private Name name;
+
+    public PersonV2(Name name) {
+        this.name = name;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonV2{" +
+                "name=" + name +
+                '}';
+    }
+}
---
+++ src/main/java/com/programmingpointer/rest/webservices/restful_web_services/versioning/VersioningPersonController.java
@@ -0,0 +1,55 @@
+package com.programmingpointer.rest.webservices.restful_web_services.versioning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VersioningPersonController {
+
+    //URI Versioning
+    @GetMapping("/v1/person")
+    public PersonV1 getFirstVersionOfPerson(){
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping("/v2/person")
+    public PersonV2 getSecondVersionOfPerson(){
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    //Request Param Versioning
+
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonRequestParam(){
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person/header", params = "version=2")
+    public PersonV2 getSecondVersionOfPersonRequestParam(){
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    //Header
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonRequestHeader(){
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionOfPersonRequestHeader(){
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    //Content-negotiation
+    @GetMapping(path = "/person/accept", produces = "application/vnd.api+json")
+    public PersonV1 getFirstVersionOfPersonAccept(){
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/json" )
+    public PersonV2 getSecondVersionOfPersonRequestAccept(){
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+}
